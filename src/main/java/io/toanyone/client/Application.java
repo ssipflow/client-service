ce6f745a48d60550b6d7cd3606e857e824c2d8c4
leonardo.park <blue.park@kt.com>
2018-03-18 23:26:28 +0900

    add ingress

--- src/main/java/io/toanyone/client/Application.java
+++ src/main/java/io/toanyone/client/Application.java
@@ -1,10 +1,15 @@
 package io.toanyone.client;
 
+import io.toanyone.client.domain.Client;
+import io.toanyone.client.repository.ClientRepository;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableCircuitBreaker
@@ -13,5 +18,29 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+
+	@Autowired
+	private ClientRepository repository;
+
+	@Bean
+	InitializingBean sendDatabase() {
+		Client client = new Client();
+
+		client.setSeq(1);
+		client.setClientId("msa");
+		client.setClientSecret("12345678");
+		client.setAuthorizedGrantTypes("client_credentials,password,refresh_token");
+		client.setScope("read,write");
+		client.setAutoApproveScopes("read,write");
+		client.setAccessTokenValiditySeconds(1000 * 60 * 5);
+		client.setRefreshTokenValiditySeconds(1000 * 60 * 5);
+		client.setAuthorities("ROLE_MOBILE_CLIENT");
+		client.setResourceIds("API");
+
+		return () -> {
+			repository.save(client);
+		};
 	}
 }
